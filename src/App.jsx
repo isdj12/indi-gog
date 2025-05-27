25cb09e05d2578ce56c16b96cd00f3cef2dfe6e2
isdj12 <artempop28@gmail.com>
2025-05-27 13:44:41 +0300

    day 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Button from './component/button.jsx'
 import { button } from './src.js'
 import Poisk from './component/poisk.jsx'
+import Profile from './component/profile.jsx'
+import screenshotImage from './assets/Screenshot_3.png'
+import reactLogo from './assets/react.svg'
 
 function App() {
   return (
     <>
       <div className="header-container">
-        <img src="/original.png" alt="original" className='logo' />
+        <img src={reactLogo} alt="logo" className='logo' />
         <div className="button-container">
           <Button text={button[0].text} href={button[0].href} />
           <Button text={button[1].text} href={button[1].href} className="right-button" />
@@ -15,9 +18,13 @@ function App() {
         </div>
         <div className="search-wrapper">
           <Poisk />
+          <Profile />
         </div>
       </div>
       <hr className='line'></hr>
+      <div>
+      <img src={screenshotImage} alt="Screenshot" className='screenshot' />
+      </div>
     </>
   )
 }
